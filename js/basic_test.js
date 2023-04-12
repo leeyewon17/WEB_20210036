acca5b839d86a78d1d373ac163607246da7fe0fc
이예원 <leeyewon1007@gmail.com>
2023-04-12 02:14:01 +0000

    6주차 수업

--- js/basic_test.js
+++ js/basic_test.js
@@ -1,20 +1,31 @@
-var jb = 'hi'; // 변수 선언 후 주석 가능
-var a = 1;
-var b;
-b = 5;
+let number = 5;
+let str = ‘문자열 입력’; // “ “도 묶음 가능
+let prime = 1.5123;
+let is_ok = true; // 참
+let is_not = false; // 거짓
+let undefi; // 변수 이름만, 초기화 x
+let empty = null; // 비어 있음
 
-if (true) {
-  let c = 'let 접근';
-  var c_1 = 'var 접근';
-}
-//console.log(c); // Error?
-console.log(c_1);
+console.log(undefi, empty); // 여러 개 출력
 
-let d = 5;
-//let d = '값 재할당'; // Error?
-console.log(d);
 
-const e = '상수1 접근';
-//e = 5;
-//const f  // Error?
-console.log(e);
+// var jb = 'hi'; // 변수 선언 후 주석 가능
+// var a = 1;
+// var b;
+// b = 5;
+
+// if (true) {
+//   let c = 'let 접근';
+//   var c_1 = 'var 접근';
+// }
+// //console.log(c); // Error?
+// console.log(c_1);
+
+// let d = 5;
+// //let d = '값 재할당'; // Error?
+// console.log(d);
+
+// const e = '상수1 접근';
+// //e = 5;
+// //const f  // Error?
+// console.log(e);
