cecf3f24f0e7a2892be90fb6fda4c84b7e693a2c
이예원 <leeyewon1007@gmail.com>
2023-05-24 02:31:37 +0000

    12주차 수업

--- js/login.js
+++ js/login.js
@@ -26,31 +26,31 @@ function login(){
 }
 
 function logout(){
+    session_del(); // 세션 삭제
     location.href="../index.html";
 }
 
 function get_id(){
-    var getParameters = function(paramName){ // 변수 = 함수(이름)
-    var returnValue; // 리턴값을 위한 변수 선언
-    var url = location.href; // 현재 접속 중인 주소 정보 저장
-    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
-        for(var i = 0; i < parameters.length; i++) { 
-		    var varName = parameters[i].split('=')[0];
-            
-            if (varName.toUpperCase() == paramName.toUpperCase()) {
-                returnValue = parameters[i].split('=')[1];
-                return decodeURIComponent(returnValue);
-            // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
-		    }
-	    } // 2중 for문 끝
-}; // 함수 끝
-alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
-}
+    if(true){
+        decrypt_text();
+    }
+    else{
+        var getParameters = function(paramName){ // 변수 = 함수(이름)
+        var returnValue; // 리턴값을 위한 변수 선언
+        var url = location.href; // 현재 접속 중인 주소 정보 저장
+        var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
+            for(var i = 0; i < parameters.length; i++) { 
+                var varName = parameters[i].split('=')[0];
 
-function deleteCookie(cookieName){
-    var expireDate = new Date();
-    expireDate.setDate(expireDate.getDate() - 1);
-    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+                if (varName.toUpperCase() == paramName.toUpperCase()) {
+                    returnValue = parameters[i].split('=')[1];
+                    return decodeURIComponent(returnValue);
+                // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
+                }
+            } // 2중 for문 끝
+    }; // 함수 끝
+    alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+    }
 }
 
 function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
@@ -62,24 +62,17 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
     id.value = get_id; 
     check.checked = true; 
     }
+    session_check(); // 세션 유무 검사
 }
 
-function session_set() { //세션 저장
-    let id = document.querySelector("#floatingInput");
-    if (sessionStorage) {
-        sessionStorage.setItem("Session_Storage_test", id.value);
 
-    } 
-    else {
-        alert("로컬 스토리지 지원 x");
-    }
+function addJavascript(jsname) { // 자바스크립트 외부 연동
+	var th = document.getElementsByTagName('head')[0];
+	var s = document.createElement('script');
+	s.setAttribute('type','text/javascript');
+	s.setAttribute('src',jsname);
+	th.appendChild(s);
 }
-
-function session_get() { //세션 읽기
-    if (sessionStorage) {
-       return sessionStorage.getItem("Session_Storage_test");
-    } 
-    else {
-        alert("세션 스토리지 지원 x");
-    }
-}
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수
