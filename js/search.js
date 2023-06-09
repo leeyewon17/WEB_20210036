67cabfc82ac09a08305539bb386b93ecfe3a93d9
이예원 <leeyewon1007@gmail.com>
2023-06-09 13:09:20 +0000

    ㅐㅐ

--- js/search.js
+++ js/search.js
@@ -23,22 +23,23 @@ function search_message() {
    if (search_str.value.length === 0) {
       alert("검색어가 비었습니다. 입력해주세요");
       }
-   
+   else{
+    let isRestrictedWords ;
    // 필터링 기능 추가
    for (let i = 0; i < restrictedWords.length; i++) {
-       if (search_str.value.includes(restrictedWords[i])) { // 검색어에 제한 단어가 포함되면
-           alert("검색어에 제한된 단어가 포함되어 있습니다. 다른 검색어를 입력해주세요.");
-          
-           return; // 검색 중단
+       if (search_str.value.length === (restrictedWords[i])) { // 검색어에 제한 단어가 포함되면
+              isRestrictedWords = true;     
+                    break;
        }
-   }
-   
-   else{
+   }   
+    if(restrictedWords){
+        alert("검색어에 제한된 단어가 포함되어 있습니다. 다른 검색어를 입력해주세요.");
+    }
+  else{
        alert("검색을 수행합니다!");
-   let text = document.getElementById("search_message").innerHTML = search_str.value;
-   document.querySelector("#form_main").submit();
-   // 검색을 위한 추가적인 동작 수행
-   }
-   // 원래 페이지에 머무르도록 처리
-   ///event.preventDefault();
+       //let text = document.getElementById("search_message").innerHTML = search_str.value;
+       document.querySelector("#form_main").submit();
+   
+     }
+   }3
 }
