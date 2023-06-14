64607b26e7269fc94fae75e366b966ac51e9ecd5
이예원 <leeyewon1007@gmail.com>
2023-06-14 02:04:36 +0000

    수업 보충내용

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+function over(obj) {
+    obj.src="img/google_valentines-day.gif"
+}
+
+function out (obj) {
+    obj.src="img/googlelogo_color_big.png"
+}
--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
-        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.38022250281632, 126.92713949502843), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
